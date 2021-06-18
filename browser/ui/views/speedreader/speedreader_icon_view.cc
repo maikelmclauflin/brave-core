c3be73efdb1e264032b261e9fae712d6dc8f0d3d
Mario Sanchez Prada <mario@igalia.com>
2021-06-18 18:26:38 +0200

    Adapt to views::Button::ink_drop() -> views::InkDrop::Get(view) change
    We should now use a class method instead of an instance one.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/a61eda05062d20ec0f592415f3a145cdf1dce095
    
    commit a61eda05062d20ec0f592415f3a145cdf1dce095
    Author: Peter Boström <pbos@chromium.org>
    Date:   Mon Jun 7 18:20:22 2021 +0000
    
        Make InkDrop a View class property
    
        Removes views::Button::ink_drop() in favor of views::InkDrop::Get(view),
        which works equally for all Views (not just Buttons).
    
        The InkDrop is removed in the destructor for all classes that override
        Add/RemoveLayerBeneathView() to prevent using the wrong overrides
        during ~View() when class properties destruct.
    
        This also removes the `destroying_` bool from InkDropHost so that layers
        can be properly removed on InkDropHost destruction, as the View may
        still live after this point on.
    
        Bug: 931964

--- browser/ui/views/speedreader/speedreader_icon_view.cc
+++ browser/ui/views/speedreader/speedreader_icon_view.cc
@@ -24,6 +24,7 @@
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/theme_provider.h"
+#include "ui/views/animation/ink_drop.h"
 #include "ui/views/animation/ink_drop_host_view.h"
 #include "ui/views/animation/ink_drop_state.h"
 
@@ -56,8 +57,10 @@ void SpeedreaderIconView::UpdateImpl() {
     return;
   }
 
-  if (ink_drop()->GetHighlighted() && !IsBubbleShowing())
-    ink_drop()->AnimateToState(views::InkDropState::HIDDEN, nullptr);
+  if (views::InkDrop::Get(this)->GetHighlighted() && !IsBubbleShowing()) {
+    views::InkDrop::Get(this)->AnimateToState(views::InkDropState::HIDDEN,
+                                              nullptr);
+  }
 
   auto* tab_helper =
       speedreader::SpeedreaderTabHelper::FromWebContents(contents);
