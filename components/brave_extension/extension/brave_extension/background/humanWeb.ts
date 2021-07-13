7e34e60a521f00f67d67482c50094df6be1850ed
Kevin Kuehler <kevin@segfault.fun>
2021-07-13 16:16:43 -0700

    Implement chrome.braveHumanWeb.getEnabled()
    We use the CLIQZ global that is injected into the background process to
    enable or disable humanweb, depending on which is set.

--- components/brave_extension/extension/brave_extension/background/humanWeb.ts
+++ components/brave_extension/extension/brave_extension/background/humanWeb.ts
@@ -1,3 +1,12 @@
+declare const CLIQZ: any; // this is a global injected by humanweb
+
 chrome.braveHumanWeb.getEnabled((enabled: boolean) => {
-  console.log("debug: ", enabled);
+  const prefs = CLIQZ.app.prefs;
+  if (enabled) {
+    prefs.set("modules.human-web.enabled", true);
+    prefs.set("modules.hpnv2.enabled", true);
+  } else {
+    prefs.set("modules.human-web.enabled", false);
+    prefs.set("modules.hpnv2.enabled", false);
+  }
 })
